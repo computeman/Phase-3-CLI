42ea34e44e88706c0b311d60dda3f8ceb0b32064
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-12-13 18:37:46 +0300

    Configure environment, create database

---
+++ lib/migrations/versions/00815cdf52f6_initial_initialization_create_db.py
@@ -0,0 +1,24 @@
+"""Initial initialization, create db
+
+Revision ID: 00815cdf52f6
+Revises: 
+Create Date: 2023-12-13 18:35:52.539606
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '00815cdf52f6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
