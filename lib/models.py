f54336e275c6fa4e6e0f9c6cb79cf516dd74eef4
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-12-13 18:53:52 +0300

    Add user model

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
-from sqlalchemy import create_engine
+from datetime import datetime
+from sqlalchemy import create_engine, desc
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine("sqlite:///taskmanager.db")
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<User(user_id={self.user_id}, username='{self.username}')>"
